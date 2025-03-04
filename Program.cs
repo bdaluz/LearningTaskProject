6b6fc7b15142871aa1fe0068df75fb4e8937d000
bdaluz <143043482+bdaluz@users.noreply.github.com>
2025-03-04 17:44:52 -0300

    Added a menu for a better terminal experience.

--- Program.cs
+++ Program.cs
@@ -14,28 +14,189 @@ async Task RunProgram()
 
     await ShowTasks(todoservice);
 
-    Console.WriteLine("\nAdd a task\n");
-    Console.Write("Title: ");
-    string title = Console.ReadLine();
-    Console.Write("Description: ");
-    string description = Console.ReadLine();
-
-    await todoservice.AddTask(title, description);
-    await ShowTasks(todoservice);
+    while (true)
+    {
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1 - Add a task");
+        Console.WriteLine("2 - Edit a task");
+        Console.WriteLine("3 - Delete a task");
+        Console.WriteLine("4 - Mark task as completed");
+        Console.WriteLine("5 - Show all tasks");
+        Console.Write("\nSelect an option: ");
+        try
+        {
+            int choice = int.Parse(Console.ReadLine());
+            if (new List<int> { 1, 2, 3, 4, 5 }.Contains(choice))
+            {
+                Console.WriteLine();
+                switch (choice)
+                {
+                    case 1:
+                        await AddTask(todoservice);
+                        break;
+                    case 2:
+                        await EditTask(todoservice);
+                        break;
+                    case 3:
+                        await RemoveTask(todoservice);
+                        break;
+                    case 4:
+                        await CompleteTask(todoservice);
+                        break;
+                    case 5:
+                        await ShowTasks(todoservice);
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid option.\n");
+            }
+        }
+        catch (Exception ex) { Console.WriteLine("\nInvalid option.\n"); }
+    }
 }
+
+static async Task AddTask(ToDoTaskService todoservice)
+{
+    Console.WriteLine("Add a task\n");
+    Console.Write("Title: ");
+    string title = Console.ReadLine().Trim();
+    Console.Write("Description: ");
+    string description = Console.ReadLine().Trim();
+    Console.WriteLine();
+    await todoservice.AddTask(title, description);
+    Console.WriteLine("New task added successfully.");
+}
+
+
+static async Task EditTask(ToDoTaskService todoservice)
+{
+    Console.Clear();
+    await ShowTasks(todoservice);
+    Console.WriteLine();
+    Console.WriteLine("Edit a task\n");
+    Console.Write("Enter the ID of the task you'd like to edit: ");
+    string id = Console.ReadLine().Trim();
+    try
+    {
+        if (!await todoservice.DoesExist(int.Parse(id)))
+        {
+            Console.WriteLine("Cannot edit a task that doesn't exist.\n");
+            return;
+        }
+
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine(e.Message);
+        return;
+    }
+    Console.Write("New Title: ");
+    string title = Console.ReadLine().Trim();
+    Console.Write("New Description: ");
+    string description = Console.ReadLine().Trim();
+    Console.WriteLine();
+    try
+    {
+        await todoservice.EditTask(int.Parse(id), title, description);
+        Console.WriteLine("Task was edited successfully.");
+    }
+    catch (InvalidOperationException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+static async Task RemoveTask(ToDoTaskService todoservice)
+{
+    Console.Clear();
+    await ShowTasks(todoservice);
+    Console.WriteLine();
+    Console.WriteLine("Edit a task\n");
+    Console.Write("Enter the ID of the task you'd like to remove: ");
+    string id = Console.ReadLine().Trim();
+    try
+    {
+        if (!await todoservice.DoesExist(int.Parse(id)))
+        {
+            Console.WriteLine("Cannot remove a task that doesn't exist.\n");
+            return;
+        }
+        else { 
+            await todoservice.RemoveTask(int.Parse(id));
+        }
+
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine(e.Message);
+        return;
+    }
+    catch (InvalidOperationException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+static async Task CompleteTask(ToDoTaskService todoservice)
+{
+    Console.Clear();
+    await ShowTasks(todoservice);
+    Console.WriteLine();
+    Console.WriteLine("Complete a task\n");
+    Console.Write("Enter the ID of the task you'd like to mark as completed: ");
+    string id = Console.ReadLine().Trim();
+    try
+    {
+        if (!await todoservice.DoesExist(int.Parse(id)))
+        {
+            Console.WriteLine("Cannot complete a task that doesn't exist.\n");
+            return;
+        }
+        else
+        {
+            await todoservice.MarkAsComplete(int.Parse(id));
+        }
+
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine(e.Message);
+        return;
+    }
+    catch (InvalidOperationException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
 
 static async Task ShowTasks(ToDoTaskService todoservice)
 {
-    Console.WriteLine("All tasks:");
+    Console.WriteLine("All tasks:\n");
     var tasks = await todoservice.GetAllTasks();
     if (tasks.Any())
     {
         foreach (var task in tasks)
         {
-            Console.WriteLine($"Id: {task.Id}, \nTitle: {task.Title}\nDescription: {task.Description}\nStatus: {(task.IsCompleted ? "Completed" : "Pending")}");
+            Console.WriteLine($"Id: {task.Id}, \nTitle: {task.Title}\nDescription: {task.Description}\nStatus: {(task.IsCompleted ? "Completed" : "Pending")}\n");
         }
     }
-    else {
+    else
+    {
         Console.WriteLine("No task found.\n");
     }
 }
--- Services/ToDoTaskService.cs
+++ Services/ToDoTaskService.cs
@@ -3,10 +3,11 @@
 using Microsoft.EntityFrameworkCore;
 using ProjetoTasks.Data;
 using ProjetoTasks.Models;
+using System.Diagnostics.Metrics;
 
 namespace ProjetoTasks.Services
 {
-    internal class ToDoTaskService (ApplicationDbContext Context)
+    internal class ToDoTaskService(ApplicationDbContext Context)
     {
         //private ApplicationDbContext _context;
         //public ToDoTaskService(ApplicationDbContext context)
@@ -14,38 +15,66 @@ namespace ProjetoTasks.Services
         //    _context = context;
         //}
 
-
         public async Task AddTask(string title, string description)
         {
             var toDoTask = new ToDoTask(title, description);
             await Context.ToDoTasks.AddAsync(toDoTask);
             await Context.SaveChangesAsync();
         }
-        public async Task EditTask(int id, string title, string description) {
+        public async Task EditTask(int id, string title, string description)
+        {
             var todotask = await Context.ToDoTasks.FindAsync(id);
-            todotask.Title = title;
-            todotask.Description = description;
-            Context.Update(todotask);
-            await Context.SaveChangesAsync();
+            if (todotask != null)
+            {
+                todotask.Title = title;
+                todotask.Description = description;
+                Context.Update(todotask);
+                await Context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot edit a task that doesn't exist.");
+            }
         }
-        public async Task RemoveTask(int id) {
+        public async Task RemoveTask(int id)
+        {
             var todotask = await Context.ToDoTasks.FindAsync(id);
-            Context.Remove(todotask);
-            await Context.SaveChangesAsync();
+            if (todotask != null)
+            {
+                Context.Remove(todotask);
+                await Context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot remove a task that doesn't exist.");
+            }
         }
 
         public async Task MarkAsComplete(int id)
         {
             var todotask = await Context.ToDoTasks.FindAsync(id);
-            todotask.IsCompleted = true;
-            Context.Update(todotask);
-            await Context.SaveChangesAsync();
+            if (todotask != null)
+            {
+                todotask.IsCompleted = true;
+                Context.Update(todotask);
+                await Context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot complete a task that doesn't exist.");
+            }
         }
 
         public async Task<List<ToDoTask>> GetAllTasks()
         {
             var allTasks = await Context.ToDoTasks.ToListAsync();
             return allTasks;
+        }
+
+        public async Task<bool> DoesExist(int id)
+        {
+            var todotask = await Context.ToDoTasks.FindAsync(id);
+            return todotask != null;
         }
     }
 }
